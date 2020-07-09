898ddb339cf12935500e61369aa09746a522f72c
mattbocc <60942416+mattbocc@users.noreply.github.com>
2020-07-09 13:02:59 -0400

    Bug Fixes

--- JS/app.js
+++ JS/app.js
@@ -24,32 +24,109 @@ window.addEventListener("load", ()=>{
                     return response.json();
                 })
                 .then(data =>{
-                    console.log(data);
 
                     tempDeg.textContent = Math.round(data.main.temp - 273.15);
                     
                     
-                    const weather = data.weather[0].description
+                    const weather = data.weather[0].description;
                     tempDesc.textContent = weather.charAt(0).toUpperCase() + weather.slice(1);
 
-                    const ic = data.weather[0].icon;
-                    const icon = `http://openweathermap.org/img/wn/${ic}@2x.png`;
-                    img.src = icon;
-                    
-                    
+                    // const ic = data.weather[0].icon;
+
+                    // const icon = `http://openweathermap.org/img/wn/${ic}@2x.png`;
+
+                    // console.log(icon);
+                    // img.src = icon;
                     
                     const time = new Date(data.dt  * 1000);//mulitply by 1000 to convert from seconds to milliseconds
                     let hours = time.getHours();
                     const minutes = time.getMinutes();
                     
+                    //picture display, getting sunrise and sun set to figure out when it's dark, to display the proper pictures
+                    
+                    const sunrise = new Date(data.sunrise * 1000);
+                    const sunriseHour = sunrise.getHours();
+                    const sunset = new Date(data.sunset * 1000);
+                    const sunsetHour = sunset.getHours();
 
-                    if(hours > 12){
+                    if(hours > sunriseHour && hours < sunsetHour){//during the day
+
+                        if(data.weather[0].main == "clear"){
+                            img.src = "pics/oned.png";
+                        }
+                        else if(data.weather[0].main == "thunderstorm"){
+                            img.src = "pics/elevend.png";
+                        }
+                        else if(data.weather[0].main == "drizzle"){
+                            img.src = "pics/nined.png";
+                        }
+                        else if(data.weather[0].main == "rain"){
+                            img.src = "pics/tend.png";
+                        }
+                        else if(data.weather[0].main == "snow"){
+                            img.src = "pics/thirteend.png";
+                        }
+                        else if( 800 > data.weather[0].id > 700 ){
+                            img.src = "pics/fiftyd.png";
+                        }
+                        else if(data.weather[0].main == "clouds"){
+                            img.src = "pics/twod.png";
+                        }
+                       
+
+                    }
+                    else{
+
+                        if(data.weather[0].main == "clear"){
+                            img.src = "pics/onen.png";
+                        }
+                        else if(data.weather[0].main == "thunderstorm"){
+                            img.src = "pics/elevend.png";
+                        }
+                        else if(data.weather[0].main == "drizzle"){
+                            img.src = "pics/nined.png";
+                        }
+                        else if(data.weather[0].main == "rain"){
+                            img.src = "pics/tenn.png";
+                        }
+                        else if(data.weather[0].main == "snow"){
+                            img.src = "pics/thirteend.png";
+                        }
+                        else if( 800 > data.weather[0].id > 700 ){
+                            img.src = "pics/fiftyd.png";
+                        }
+                        else if(data.weather[0].main == "clouds"){
+                            img.src = "pics/twon.png";
+                        }
+                       
+
+                    }
+
+                    //time display
+
+                    if(hours == 12){//gives the proper time right at noon
+                        if(minutes < 10){
+                            timeDis.textContent = `${hours}:0${minutes} PM`;
+                        }
+                        else{
+                            timeDis.textContent = `${hours}:${minutes} PM`;
+                        }
+                    }
+                    else if(hours > 12){
                         hours = hours - 12;
                         if(minutes < 10){
                             timeDis.textContent = `${hours}:0${minutes} PM`;
                         }
                         else{
                             timeDis.textContent = `${hours}:${minutes} PM`;
+                        }
+                    }
+                    else if(hours == 0){//gives 0 hours at midnight (military time), this is a workaround for it
+                        if(minutes < 10){
+                            timeDis.textContent = `12:0${minutes} AM`;
+                        }
+                        else{
+                            timeDis.textContent = `12:${minutes} AM`;
                         }
                     }
                     else{
@@ -85,4 +162,5 @@ window.addEventListener("load", ()=>{
     else{
         alert('Please refresh and allow location');
     }
+
 });
